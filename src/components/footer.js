fdaeaa8e4e59a9abb5e58b44975c23a4912d9ecc
unknown <ricardo.goncalves252@gmail.com>
2019-11-27 22:36:31 +0000

    blank target on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => (
   <footer>
-    <p>Developed by <Link href="https://riagoncalves.dev/"><a>riagoncalves.dev</a></Link></p>
+    <p>Developed by <Link href="https://riagoncalves.dev/"><a target="_blank">riagoncalves.dev</a></Link></p>
   </footer>
 );
 
