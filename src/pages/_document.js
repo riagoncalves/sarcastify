a3c0190b15f0ec1e20ec2606e11d79fd45c8f0d8
riagoncalves <ricardo.goncalves252@gmail.com>
2019-11-27 14:04:31 +0000

    google adsense script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,16 @@ export default class CustomDocument extends Document {
           <Header/>
           <Main />
           <NextScript />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: "ca-pub-4462855175615535",
+                  enable_page_level_ads: true
+                });`
+            }}
+          />
         </body>
       </html>
     );
