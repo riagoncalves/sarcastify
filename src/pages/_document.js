87c65ccac57ba04e8d2d0fe3df7d4ab0af69605e
unknown <ricardo.goncalves252@gmail.com>
2021-01-30 03:14:38 +0000

    added gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,19 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head/>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-R7J594LTNN"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: 
+                `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+
+                gtag('config', 'G-R7J594LTNN');`
+            }}
+          />
+        </Head>
         <body>
           <Header/>
           <Main/>
